098dc44aac0733080679405cc28f825226390ba3
Qrexx <josephheinz28@gmail.com>
2024-08-31 21:28:43 -0400

    new microblog post

--- microblog/main.js
+++ microblog/main.js
@@ -3,7 +3,8 @@ const titleDOM = document.querySelector("#title");
 const feedDOM = document.querySelector("#feed");
 const posts = [
   { time: "08:53p", date: "08/31", content: "microbloggin'", id: 1 },
-  { time: "09:23p", date: "08/31", content: "damn son, where'd you find this?", id: 2 }
+  { time: "09:23p", date: "08/31", content: "damn son, where'd you find this?", id: 2 },
+  { time: "09:26p", date: "08/31", content: "my website is so stupid, a windows-esque homepage, a terminal themed blog, and a clean neumorphism microblog", id: 3 },
 ];
 const clamp = (input, min, max) => {
   if (input < min) {
